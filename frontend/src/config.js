b76910b82e1545d04fed03325866180cb11c6245
Lmatty110 <aquilareale.mz@gmail.com>
2026-02-20 17:18:01 +0100

    config con /api

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+
+export const API_BASE_URL = `${BASE_URL}/api`;
